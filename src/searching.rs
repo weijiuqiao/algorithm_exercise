dd34a5c05c649c7a87f266d24d54ccc1eec65bfa
weijiuqiao <weijiuqiao@foxmail.com>
2020-04-29 17:03:51 +0800

    move option inside of refcell

--- src/searching.rs
+++ src/searching.rs
@@ -2,15 +2,15 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 pub struct BinarySearchTree<T: PartialOrd + Copy, U> {
-    root: Option<Rc<RefCell<BTNode<T, U>>>>,
+    root: Rc<RefCell<Option<BTNode<T, U>>>>,
 }
 
 /// Binary search tree node
 pub struct BTNode<T: PartialOrd + Copy, U> {
     key: T,
     value: U,
-    left: Option<Rc<RefCell<BTNode<T, U>>>>,
-    right: Option<Rc<RefCell<BTNode<T, U>>>>,
+    left: Rc<RefCell<Option<BTNode<T, U>>>>,
+    right: Rc<RefCell<Option<BTNode<T, U>>>>,
     n: usize,
 }
 
@@ -19,8 +19,8 @@ impl<T: PartialOrd + Copy, U: Clone> BTNode<T, U> {
         BTNode {
             key,
             value,
-            left: None,
-            right: None,
+            left: Rc::new(RefCell::new(None)),
+            right: Rc::new(RefCell::new(None)),
             n,
         }
     }
@@ -29,7 +29,9 @@ impl<T: PartialOrd + Copy, U: Clone> BTNode<T, U> {
 impl<T: PartialOrd + Copy, U: Clone> BinarySearchTree<T, U> {
     /// Initializer.
     pub fn new() -> Self {
-        BinarySearchTree { root: None }
+        BinarySearchTree {
+            root: Rc::new(RefCell::new(None)),
+        }
     }
 
     /// Return size of the tree, ie. count of nodes.
@@ -40,8 +42,8 @@ impl<T: PartialOrd + Copy, U: Clone> BinarySearchTree<T, U> {
     /// assert_eq!(tree.size(), 0);
     /// ```
     pub fn size(&self) -> usize {
-        if let Some(ref root) = self.root {
-            root.borrow().n
+        if let Some(ref root) = *self.root.borrow() {
+            root.n
         } else {
             0
         }
@@ -60,15 +62,15 @@ impl<T: PartialOrd + Copy, U: Clone> BinarySearchTree<T, U> {
         Self::get_internal(&self.root, key)
     }
 
-    fn get_internal(node: &Option<Rc<RefCell<BTNode<T, U>>>>, key: T) -> Option<U> {
-        if let Some(node) = node {
-            let ref_node = node.borrow();
-            if ref_node.key > key {
-                Self::get_internal(&ref_node.left, key)
-            } else if ref_node.key < key {
-                Self::get_internal(&ref_node.right, key)
+    fn get_internal(node: &Rc<RefCell<Option<BTNode<T, U>>>>, key: T) -> Option<U> {
+        if let Some(ref node) = *node.borrow() {
+            // let ref_node = node.borrow();
+            if node.key > key {
+                Self::get_internal(&node.left, key)
+            } else if node.key < key {
+                Self::get_internal(&node.right, key)
             } else {
-                Some(ref_node.value.clone())
+                Some(node.value.clone())
             }
         } else {
             None
@@ -76,28 +78,30 @@ impl<T: PartialOrd + Copy, U: Clone> BinarySearchTree<T, U> {
     }
 
     /// Insert key value pair into tree. If key already exists, update the corresponding value.
-    pub fn put(&mut self, key: T, val: U) {
-        BinarySearchTree::put_internal(&mut self.root, key, val);
+    pub fn put(&self, key: T, val: U) {
+        BinarySearchTree::put_internal(&self.root, key, val);
     }
 
-    fn put_internal(node: &mut Option<Rc<RefCell<BTNode<T, U>>>>, key: T, val: U) {
-        if let Some(node) = node {
-            let mut ref_node = node.borrow_mut();
-            if ref_node.key > key {
-                BinarySearchTree::put_internal(&mut ref_node.left, key, val);
-            } else if ref_node.key < key {
-                BinarySearchTree::put_internal(&mut ref_node.right, key, val);
+    fn put_internal(node: &Rc<RefCell<Option<BTNode<T, U>>>>, key: T, val: U) {
+        let mut m = node.borrow_mut();
+        if let Some(ref mut node) = *m {
+            // let mut ref_node = node.borrow_mut();
+            if node.key > key {
+                BinarySearchTree::put_internal(&node.left, key, val);
+            } else if node.key < key {
+                BinarySearchTree::put_internal(&node.right, key, val);
             } else {
-                ref_node.value = val;
+                node.value = val;
             }
-            ref_node.n = Self::node_size(&ref_node.left) + Self::node_size(&ref_node.right) + 1;
+            node.n = Self::node_size(&node.left) + Self::node_size(&node.right) + 1;
         } else {
-            *node = Some(Rc::new(RefCell::new(BTNode::new(key, val, 1)))); //Some(Box::new(BTNode::new(key, val, 1)));
+            *m = Some(BTNode::new(key, val, 1));
         }
     }
-    fn node_size(node: &Option<Rc<RefCell<BTNode<T, U>>>>) -> usize {
-        if let Some(node) = node {
-            node.borrow().n
+
+    fn node_size(node: &Rc<RefCell<Option<BTNode<T, U>>>>) -> usize {
+        if let Some(ref node) = *node.borrow() {
+            node.n
         } else {
             0
         }
@@ -112,163 +116,137 @@ impl<T: PartialOrd + Copy, U: Clone> BinarySearchTree<T, U> {
     /// assert_eq!(tree.min(), Some("one"))
     /// ```
     pub fn min(&self) -> Option<T> {
-        match &self.root {
-            None => None,
-            Some(node) => Some(Self::min_internal(node.clone()).borrow().key),
-        }
-    }
-
-    fn min_internal(node: Rc<RefCell<BTNode<T, U>>>) -> Rc<RefCell<BTNode<T, U>>> {
-        match &node.borrow().left {
-            None => node.clone(),
-            Some(left) => Self::min_internal((*left).clone()),
-        }
-    }
-
-    /// Get the key of the floor node of `key`, i.e. the largest key in the BST less than or equal to `key`.
-    pub fn floor(&self, key: T) -> Option<T> {
-        match &self.root {
-            Some(rc_node) => match Self::floor_internal(Some((*rc_node).clone()), key) {
-                Some(ref_node) => Some(ref_node.borrow().key),
-                None => None,
-            },
-            None => None,
-        }
-        // match Self::floor_internal(self.root, key) {
-        //     Some(boxed_node) => Some(boxed_node.borrow().key),
+        // match *Self::min_internal(self.root.clone()).borrow() {
+        //     Some(ref node) => Some(node.key),
         //     None => None,
         // }
+        Self::min_internal(self.root.clone()) // Rc<RefCell<Option<Node>>>
+            .borrow() // &Option<Node>
+            .as_ref() // Option<&Node> (auto dereffed)
+            .map(|x| x.key) // Option<T> (T copied)
+    }
+
+    fn min_internal(node: Rc<RefCell<Option<BTNode<T, U>>>>) -> Rc<RefCell<Option<BTNode<T, U>>>> {
+        match *node.borrow() {
+            None => node.clone(),
+            Some(ref b_node) => match *b_node.left.borrow() {
+                None => node.clone(),
+                Some(_) => Self::min_internal(b_node.left.clone()),
+            },
+        }
+    }
+    /// Get the key of the floor node of `key`, i.e. the largest key in the BST less than or equal to `key`.
+    pub fn floor(&self, key: T) -> Option<T> {
+        Self::floor_internal(self.root.clone(), key) // Rc<RefCell<Option<Node>>>
+            .borrow() // &Option<Node>
+            .as_ref() // Option<&Node> (auto dereffed)
+            .map(|x| x.key) // Option<T> (T copied)
     }
 
     fn floor_internal(
-        node: Option<Rc<RefCell<BTNode<T, U>>>>,
+        node: Rc<RefCell<Option<BTNode<T, U>>>>,
         key: T,
-    ) -> Option<Rc<RefCell<BTNode<T, U>>>> {
-        match node {
-            Some(box_node) => {
-                if key < box_node.borrow().key {
-                    match &box_node.borrow().left {
-                        Some(left) => Self::floor_internal(Some((*left).clone()), key),
-                        None => None,
-                    }
-                } else if key > box_node.borrow().key {
-                    let t = match &box_node.borrow().right {
-                        Some(right) => Self::floor_internal(Some((*right).clone()), key),
-                        None => None,
-                    };
-                    match t {
-                        Some(_) => t,
-                        None => Some(box_node),
+    ) -> Rc<RefCell<Option<BTNode<T, U>>>> {
+        match *node.borrow() {
+            Some(ref b_node) => {
+                if key < b_node.key {
+                    Self::floor_internal(b_node.left.clone(), key)
+                } else if key > b_node.key {
+                    let floor_right = Self::floor_internal(b_node.right.clone(), key);
+                    match *floor_right.clone().borrow() {
+                        Some(_) => floor_right,
+                        None => node.clone(),
                     }
                 } else {
-                    Some(box_node)
+                    node.clone()
                 }
             }
-            None => None,
+            None => node.clone(),
         }
     }
-
 
     /// Get the key of the node of rank `rank` (the key such that
     /// precisely *rank* other keys in the BST are smaller).
     pub fn key_of_rank(&self, rank: usize) -> Option<T> {
-        match &self.root {
-            Some(rc_node) => match Self::select_internal(Some((*rc_node).clone()), rank) {
-                Some(ref_node) => Some(ref_node.borrow().key),
-                None => None,
-            },
-            None => None,
-        }
-        // match Self::select_internal(self.root, rank) {
-        //     Some(boxed_node) => Some(boxed_node.borrow().key),
-        //     None => None,
-        // }
+        Self::select_internal(self.root.clone(), rank)
+            .borrow()
+            .as_ref()
+            .map(|x| x.key)
     }
 
     fn select_internal(
-        node: Option<Rc<RefCell<BTNode<T, U>>>>,
+        node: Rc<RefCell<Option<BTNode<T, U>>>>,
         rank: usize,
-    ) -> Option<Rc<RefCell<BTNode<T, U>>>> {
-        match node {
-            Some(rc_node) => {
-                let t = Self::node_size(&rc_node.borrow().left);
+    ) -> Rc<RefCell<Option<BTNode<T, U>>>> {
+        match *node.borrow() {
+            Some(ref b_node) => {
+                let t = Self::node_size(&b_node.left);
                 if t > rank {
-                    match &rc_node.borrow().left {
-                        Some(left) => Self::select_internal(Some((left).clone()), rank),
-                        None => None,
-                    }
-                // Self::select_internal((*(rc_node).clone()).borrow().left, rank)
+                    Self::select_internal(b_node.left.clone(), rank)
                 } else if t < rank {
-                    match &rc_node.borrow().right {
-                        Some(right) => Self::select_internal(Some((right).clone()), rank - t - 1),
-                        None => None,
-                    }
-                // Self::select_internal(rc_node.borrow().right, rank - t - 1)
+                    Self::select_internal(b_node.right.clone(), rank - t - 1)
                 } else {
-                    Some(rc_node)
+                    node.clone()
                 }
             }
-            None => None,
+            None => node.clone(),
         }
     }
-
     /// Inverse method of `key_of_rank()`.
     pub fn rank_of_key(&self, key: T) -> usize {
-        return Self::rank_internal(self.root.as_ref(), key);
+        return Self::rank_internal(self.root.clone(), key);
     }
 
-    fn rank_internal(node: Option<&Rc<RefCell<BTNode<T, U>>>>, key: T) -> usize {
-        match node {
-            Some(rc_node) => {
-                let ref_node = rc_node.borrow();
-                if key < ref_node.key {
-                    Self::rank_internal(ref_node.left.as_ref(), key)
-                } else if key > ref_node.key {
-                    1 + Self::node_size(&ref_node.left)
-                        + Self::rank_internal(ref_node.right.as_ref(), key)
+    fn rank_internal(node: Rc<RefCell<Option<BTNode<T, U>>>>, key: T) -> usize {
+        match *node.borrow() {
+            Some(ref b_node) => {
+                if key < b_node.key {
+                    Self::rank_internal(b_node.left.clone(), key)
+                } else if key > b_node.key {
+                    1 + Self::node_size(&b_node.left)
+                        + Self::rank_internal(b_node.right.clone(), key)
                 } else {
-                    Self::node_size(&ref_node.left)
+                    Self::node_size(&b_node.left)
                 }
             }
             None => 0,
         }
     }
-
     pub fn delete_min(&mut self) {
-        match &self.root {
-            Some(rc_node) => self.root = Self::delete_min_internal(Some((*rc_node).clone())),
-            None => (),
-        }
+        self.root = Self::delete_min_internal(self.root.clone())
     }
 
     fn delete_min_internal(
-        node: Option<Rc<RefCell<BTNode<T, U>>>>,
-    ) -> Option<Rc<RefCell<BTNode<T, U>>>> {
-        match node {
-            Some(rc_node) => {
-                if rc_node.borrow().left.is_none() {
-                    match &rc_node.borrow().right {
-                        Some(right) => Some(right.clone()),
-                        None => None,
-                    }
+        node: Rc<RefCell<Option<BTNode<T, U>>>>,
+    ) -> Rc<RefCell<Option<BTNode<T, U>>>> {
+        match *node.borrow_mut() {
+            Some(ref mut b_node) => {
+                if b_node.left.borrow().is_none() {
+                    b_node.right.clone()
                 } else {
-                    let new_left = match &rc_node.borrow().left {
-                        Some(left) => Self::delete_min_internal(Some(left.clone())),
-                        None => None,
-                    };
-                    {
-                        rc_node.borrow_mut().left = new_left;
-                    }
-                    let new_n = {
-                        Self::node_size(&rc_node.borrow().left)
-                            + Self::node_size(&rc_node.borrow().right)
-                            + 1
-                    };
-                    rc_node.borrow_mut().n = new_n;
-                    Some(rc_node)
+                    b_node.left = Self::delete_min_internal(b_node.left.clone());
+                    b_node.n = Self::node_size(&b_node.left) + Self::node_size(&b_node.right) + 1;
+                    node.clone()
                 }
             }
-            None => None,
+            None => node.clone(),
+        }
+    }
+
+    pub fn delete_min_internal2(
+        node: Rc<RefCell<Option<BTNode<T, U>>>>,
+    ) -> Rc<RefCell<Option<BTNode<T, U>>>> {
+        match *node.borrow_mut() {
+            Some(ref mut b_node) => {
+                if b_node.left.borrow().is_none() {
+                    b_node.right.clone()
+                } else {
+                    b_node.left = Self::delete_min_internal(b_node.left.clone());
+                    b_node.n = Self::node_size(&b_node.left) + Self::node_size(&b_node.right) + 1;
+                    node.clone()
+                }
+            }
+            None => node.clone(),
         }
     }
 }
